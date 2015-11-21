4febe5e0524b20e6a52970f68ef20ed8c291154f
Jaxon Wright <jaxonhwright@gmail.com>
2015-11-20 23:21:47 -0500

    Added start of color selection to colors tab

--- app/src/main/java/com/dunrite/xpaper/ColorsFragment.java
+++ app/src/main/java/com/dunrite/xpaper/ColorsFragment.java
@@ -2,19 +2,21 @@ package com.dunrite.xpaper;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
-import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+
+import java.util.ArrayList;
 
 /**
  * Fragment to house everything to do with displaying the list of wallpapers
  */
 public class ColorsFragment extends Fragment {
-
-    private RecyclerView mRecyclerView;
-    private RecyclerView.Adapter mAdapter;
-    private RecyclerView.LayoutManager mLayoutManager;
+    Button frontButton, backButton, accentButton;
+    ArrayList<String> bColors = new ArrayList<>();
+    ArrayList<String> aColors = new ArrayList<>();
+    ArrayList<String> fColors = new ArrayList<>();
 
     public ColorsFragment() {
         // Required empty public constructor
@@ -28,8 +30,50 @@ public class ColorsFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.content_colors, container, false);
+        View rootView = inflater.inflate(R.layout.content_colors, container, false);
+
+        //instantiate buttons
+        frontButton = (Button) rootView.findViewById(R.id.front_button);
+        backButton = (Button) rootView.findViewById(R.id.back_button);
+        accentButton = (Button) rootView.findViewById(R.id.accent_button);
+
+        //set up onClickListeners
+        frontButton.setOnClickListener(fHandler);
+        backButton.setOnClickListener(bHandler);
+        accentButton.setOnClickListener(aHandler);
+
+
+        return rootView;
     }
+
+    /**
+     * fHandler
+     * OnClickListener for front button
+     */
+    View.OnClickListener fHandler = new View.OnClickListener() {
+        public void onClick(View v) {
+
+        }
+    };
+
+    /**
+     * bHandler
+     * OnClickListener for back button
+     */
+    View.OnClickListener bHandler = new View.OnClickListener() {
+        public void onClick(View v) {
+            // it was the 2nd button
+        }
+    };
+
+    /**
+     * aHandler
+     * OnClickListener for accent button
+     */
+    View.OnClickListener aHandler = new View.OnClickListener() {
+        public void onClick(View v) {
+            // it was the 1st button
+        }
+    };
 
 }

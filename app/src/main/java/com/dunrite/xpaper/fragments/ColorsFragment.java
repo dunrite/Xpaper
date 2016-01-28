533cfd7f65e7e58b57826c52fd5798bc4809aab5
Jaxon Wright <jaxonhwright@gmail.com>
2016-01-28 18:44:14 -0500

    Added x2013 device preview in colors fragment

--- app/src/main/java/com/dunrite/xpaper/fragments/ColorsFragment.java
+++ app/src/main/java/com/dunrite/xpaper/fragments/ColorsFragment.java
@@ -5,7 +5,6 @@ import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.content.ContextCompat;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -90,13 +89,12 @@ public class ColorsFragment extends Fragment {
      * model they are using.
      */
     public void resetColors() {
-        Log.d("Model", model);
         bColors = new ArrayList<>();
         aColors = new ArrayList<>();
-        fetchColors(bColors, model, "back");
+        fetchBackColors(bColors, model);
         int[] back = Utils.toIntArray(bColors, getContext());
 
-        fetchColors(aColors, model, "accent");
+        fetchAccentColors(aColors, model);
         int[] accent = Utils.toIntArray(aColors, getContext());
         frontChooser = new ColorChooserDialog.Builder((ColorsActivity) getActivity(), R.string.front_color)
                 .customColors(front, null)
@@ -107,6 +105,7 @@ public class ColorsFragment extends Fragment {
         accentChooser = new ColorChooserDialog.Builder((ColorsActivity) getActivity(), R.string.accent_color)
                 .customColors(accent, null)
                 .allowUserColorInput(false);
+        colorBackPreview();
     }
 
     AdapterView.OnItemSelectedListener mHandler = new AdapterView.OnItemSelectedListener() {
@@ -115,26 +114,27 @@ public class ColorsFragment extends Fragment {
             switch (position) {
                 case 0:
                     model = "PURE";
-                    resetColors();
                     Utils.saveDeviceConfig(getActivity(), position, "model", "MODEL");
+                    resetColors();
                     return;
                 case 1:
                     model = "PURE"; //The Style is essentially the same as the Pure
-                    resetColors();
                     Utils.saveDeviceConfig(getActivity(), position, "model", "MODEL");
+                    resetColors();
                     return;
                 case 2:
                     model = "2013";
-                    resetColors();
                     Utils.saveDeviceConfig(getActivity(), position, "model", "MODEL");
+                    resetColors();
                     return;
                 case 3:
                     model = "2014";
-                    resetColors();
                     Utils.saveDeviceConfig(getActivity(), position, "model", "MODEL");
+                    resetColors();
                     return;
                 default:
             }
+
         }
 
         @Override
@@ -178,17 +178,38 @@ public class ColorsFragment extends Fragment {
     };
 
     /**
-     * fetchColors
-     * gets all of the IDs for each color for specified 'model'
+     * fetchBackColors
+     * gets all of the back color IDs for each color for specified 'model'
      * and inserts them into 'list'
      */
-    public void fetchColors(ArrayList<Integer> list, String model, String type) {
+    public void fetchBackColors(ArrayList<Integer> list, String model) {
         Field[] ID_Fields = R.color.class.getFields();
         for (int i = 0; i < ID_Fields.length; i++) {
             String curr = ID_Fields[i].toString();
-            if ("PURE".equals(model) && curr.contains("pure") ||
-                    "2014".equals(model) && curr.contains("x14") ||
-                    "2013".equals(model) && curr.contains("x13")) {
+            if ("PURE".equals(model) && curr.contains("pure_") ||
+                    "2014".equals(model) && curr.contains("x14_") ||
+                    "2013".equals(model) && curr.contains("x13_")) {
+                try {
+                    list.add(ID_Fields[i].getInt(null));
+                } catch (IllegalArgumentException | IllegalAccessException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+
+    /**
+     * fetchAccentColors
+     * gets all of the accent color IDs for each color for specified 'model'
+     * and inserts them into 'list'
+     */
+    public void fetchAccentColors(ArrayList<Integer> list, String model) {
+        Field[] ID_Fields = R.color.class.getFields();
+        for (int i = 0; i < ID_Fields.length; i++) {
+            String curr = ID_Fields[i].toString();
+            if ("PURE".equals(model) && curr.contains("purea_") ||
+                    "2014".equals(model) && curr.contains("x14a_") ||
+                    "2013".equals(model) && curr.contains("x13a_")) {
                 try {
                     list.add(ID_Fields[i].getInt(null));
                 } catch (IllegalArgumentException | IllegalAccessException e) {
@@ -209,17 +230,18 @@ public class ColorsFragment extends Fragment {
             case "front":
                 frontCirc.setColorFilter(selectedColor);
                 Utils.saveDeviceConfig(getActivity(), selectedColor, "front", "COLORS");
-                return;
+                break;
             case "back":
                 backCirc.setColorFilter(selectedColor);
                 Utils.saveDeviceConfig(getActivity(), selectedColor, "back", "COLORS");
-                return;
+                break;
             case "accent":
                 accCirc.setColorFilter(selectedColor);
                 Utils.saveDeviceConfig(getActivity(), selectedColor, "accent", "COLORS");
-                return;
+                break;
             default:
         }
+        colorBackPreview();
     }
 
     /**
@@ -237,15 +259,15 @@ public class ColorsFragment extends Fragment {
 
     public void colorBackPreview() {
         int model = Utils.getModel(getActivity());
-        Drawable back = null;
-        Drawable accent = null;
+        Drawable back = ContextCompat.getDrawable(getContext(), R.drawable.pureback);
+        Drawable accent = ContextCompat.getDrawable(getContext(), R.drawable.pureaccent);
         if (model == 0 || model == 1) {
             back = ContextCompat.getDrawable(getContext(), R.drawable.pureback);
             accent = ContextCompat.getDrawable(getContext(), R.drawable.pureaccent);
-        } /*else if (model == 2) {
+        } else if (model == 2) {
             back = ContextCompat.getDrawable(getContext(), R.drawable.x13back);
             accent = ContextCompat.getDrawable(getContext(), R.drawable.x13accent);
-        } else if (mdoel == 3) {
+        } /*else if (mdoel == 3) {
             back = ContextCompat.getDrawable(getContext(), R.drawable.x14back);
             accent = ContextCompat.getDrawable(getContext(), R.drawable.x14accent);
         }*/
